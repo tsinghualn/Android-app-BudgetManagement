1a7063996eacaa8844bad98ad53a1df47a82f46c
Joon <lim62@wisc.edu>
2020-04-08 15:08:06 -0500

    main page activity basic layout

--- app/src/main/java/com/example/badgerbudget/MainPageActivity.java
+++ app/src/main/java/com/example/badgerbudget/MainPageActivity.java
@@ -2,21 +2,18 @@ package com.example.badgerbudget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.view.LineChartView;
 
 public class MainPageActivity extends AppCompatActivity {
@@ -26,13 +23,51 @@ public class MainPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        TextView currentBalenceText = (TextView) findViewById(R.id.currentBalenceText);
+        TextView currentBalanceText = (TextView) findViewById(R.id.currentBalanceText);
         TextView currentExpenseText = (TextView) findViewById(R.id.currentExpenseText);
         TextView targetExpenseText = (TextView) findViewById(R.id.targetExpenseText);
         Spinner typeSpinner = (Spinner) findViewById(R.id.typeSpinner);
         Spinner categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
 
-        //
+        // current balance
+        TextView currentBalance = new TextView(getApplicationContext());
+        String currentBalanceString = "";
+        currentBalance.setText(currentBalanceString);
+
+        // current expense
+        TextView currentExpense = new TextView(getApplicationContext());
+        String currentExpenseString = "";
+        currentExpense.setText(currentExpenseString);
+
+        // target expense
+        TextView targetExpense = new TextView(getApplicationContext());
+        String targetExpenseString = "";
+        targetExpense.setText(targetExpenseString);
+
+        // get categories
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        String[] arrayCategorySpinner = new String[] {
+                "1", "2", "3", "4", "5", "6", "7"
+        };
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrayCategorySpinner);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
+
+        // get type
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        String[] arrayTypeSpinner = new String[] {
+                "1", "2", "3", "4", "5", "6", "7"
+        };
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arrayTypeSpinner);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(typeAdapter);
+
+
+        // graph
         LineChartView lineChartView = findViewById(R.id.chart);
 
         String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
@@ -53,9 +88,6 @@ public class MainPageActivity extends AppCompatActivity {
         LineChartData data = new LineChartData();
         data.setLines(lines);
         lineChartView.setLineChartData(data);
-
-
-
 
     }
 }
