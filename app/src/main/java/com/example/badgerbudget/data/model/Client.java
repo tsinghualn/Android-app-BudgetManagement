9ddf5e465d28d7a2b7739a362b698c4bf7e36d5a
AndySupreme <43858095+AndySupreme@users.noreply.github.com>
2020-04-13 18:40:01 -0500

    Login Feature Added
    Commented out goals activity and fixed client so the server will accept it.

--- app/src/main/java/com/example/badgerbudget/data/model/Client.java
+++ app/src/main/java/com/example/badgerbudget/data/model/Client.java
@@ -1,26 +1,33 @@
+package com.example.badgerbudget.data.model;
+import android.os.AsyncTask;
+
 import java.net.*;
 import java.io.*;
 
-public class Client {
+public class Client extends AsyncTask<String,Void,String> {
 	private Socket socket;
 	private BufferedReader in;
 	private PrintWriter out;
 	private String hostname;
 	private int port;
 	private static volatile String returnValue;
-	
-	Client(int port, String hostname) {
-			this.port = port;
-			this.hostname = hostname;
-	        //new Thread(new ClientThread()).start();
+	private static volatile String message;
+
+	public Client(){}
+	public Client(int port, String hostname) {
+		this.port = port;
+		this.hostname = hostname;
+		//new Thread(new ClientThread()).start();
 	}
-		
+
 	public String sendMessage(String message) {
-		Thread thread = new Thread(new ClientThread());
+		this.message = message;
+		Thread thread = new Thread(new ClientThread(port,hostname));
 		thread.start();
 		try {
 			thread.join();
 		} catch(Exception e) {
+			e.printStackTrace();
 			System.out.println("Interrupted Exception");
 		}
 		String response = "";
@@ -34,43 +41,57 @@ public class Client {
 		}
 		return response;*/ return returnValue;
 	}
-    class ClientThread implements Runnable {
-    	 
-        @Override
-        public void run() {
- 
-            try {
-                InetAddress serverAddr = InetAddress.getByName(hostname);
-                socket = new Socket(serverAddr, port);
-                out = new PrintWriter(new BufferedWriter(
-                        new OutputStreamWriter(socket.getOutputStream())),
-                        true);
-    	        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                System.out.println(serverAddr);
-                out.println("Hello");
-                try {
-                    String response = "";
-                    response = in.readLine();
-                    System.out.println(response);
-                    returnValue = response;
-        		} catch(IOException e) {
-        			System.out.println("Could not receive response.");
-        		}
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
- 
-        }
- 
-    }
+
+	@Override
+	protected String doInBackground(String[] objects) {
+		this.port = port;
+		this.hostname = hostname;
+		return null;
+	}
+
+
+	class ClientThread implements Runnable  {
+		int port;
+		String hostname;
+		public ClientThread(int port, String hostname){
+			this.port = port;
+			this.hostname = hostname;
+		}
+		public void run() {
+			android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+
+			try {
+				System.out.println(this.hostname);
+				System.out.println(this.port);
+				InetAddress serverAddr = InetAddress.getByName(this.hostname);
+				socket = new Socket(serverAddr, this.port);
+				out = new PrintWriter(new BufferedWriter(
+						new OutputStreamWriter(socket.getOutputStream())),
+						true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				System.out.println(serverAddr);
+				out.println(message);
+				try {
+					String response = "";
+					response = in.readLine();
+					System.out.println(response);
+					returnValue = response;
+				} catch(IOException e) {
+					System.out.println("Could not receive response.");
+				}
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 	public static void main(String[] args) throws UnknownHostException {
-		InetAddress ip = InetAddress.getLocalHost();
-		String hostname = ip.getHostName();
-		Client client = new Client(6868, hostname);
-		System.out.println(client.sendMessage("hello"));
-		
+		//InetAddress ip = InetAddress.getLocalHost();
+		//String hostname = ip.getHostName();
+		//Client client = new Client(6868, hostname);
+		//System.out.println(client.sendMessage("login;andy_boho password"));
+
 	}
 }
 
