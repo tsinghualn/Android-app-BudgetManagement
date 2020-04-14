a6a8ae13505b648f726d485e8684c9953e785f58
Ali Zaidi <azaidi3@wisc.edu>
2020-04-13 18:43:44 -0700

    updated create account page to use server/client

--- app/src/main/java/com/example/badgerbudget/CreateAccountActivity.java
+++ app/src/main/java/com/example/badgerbudget/CreateAccountActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.badgerbudget.ui.login.LoginActivity;
+import com.example.badgerbudget.R;
+import com.example.badgerbudget.data.model.*;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -14,35 +24,63 @@ public class CreateAccountActivity extends AppCompatActivity {
     EditText passwordBox ;
     EditText nameBox;
     EditText birthday;
-    EditText s1q;
-    EditText s2q;
-    EditText s3q;
+    TextView s1q;
+    TextView s2q;
+    TextView s3q;
     EditText s1a;
     EditText s2a;
     EditText s3a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createaccount);
+        setContentView(R.layout.activity_main);
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         usernameBox = (EditText) findViewById(R.id.usernameBox);
         passwordBox = (EditText) findViewById(R.id.passwordBox);
         nameBox = (EditText) findViewById(R.id.nameBox);
         birthday = (EditText) findViewById(R.id.birthdayBox);
-        s1q = (EditText) findViewById(R.id.s1QuestionText);
-        s2q = (EditText) findViewById(R.id.s2QuestionText);
-        s3q = (EditText) findViewById(R.id.s3QuestionText);
+        s1q = (TextView) findViewById(R.id.s1QuestionText);
+        s2q = (TextView) findViewById(R.id.s2QuestionText);
+        s3q = (TextView) findViewById(R.id.s3QuestionText);
         s1a = (EditText) findViewById(R.id.s1AnswerText);
         s2a = (EditText) findViewById(R.id.s2answerTest);
         s3a = (EditText) findViewById(R.id.s3AnswerText);
+
+        s1q.setText("What is your mother's maiden name?");
+        s2q.setText("What is your favorite color?");
+        s3q.setText("What was the name of your best friend in 1st grade?");
+        //harddcode the theree security questions, only want to store the answers.
     }
 
-    public void createAccount(View view){
+    public void createAccount(View view) throws UnknownHostException {
         if(usernameBox.length() == 0 || passwordBox.length() == 0 || nameBox.length() == 0 || birthday.length() == 0 || s1q.length() == 0 || s2q.length() == 0 || s2a.length() == 0 || s3q.length()== 0 || s3a.length() ==0)
         {
             Toast.makeText(getApplicationContext(), "All fields must be filled before creating account!", Toast.LENGTH_LONG).show();
+            return;
         }
+        if(passwordBox.length() < 5){
+            Toast.makeText(getApplicationContext(),"Password must be at least 5 characters!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+       // String params[] = {"6868", InetAddress.getLocalHost().getHostName()};
+        Client client = new Client(6868, "10.0.2.2");
+        //client.execute(params);
+        System.out.println("Client created");
+        //if username already exists in the database, we let the user know
+        if(client.sendMessage("createuser;"+
+                usernameBox.getText() + " " + passwordBox.getText() + " " + s1a.getText() + " " + s2a.getText() + " " +
+                s3a.getText() + " " + birthday.getText() + " " + nameBox.getText()).equals("username already exists!")){
+
+            Toast.makeText(getApplicationContext(),"Username already exists!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(getApplicationContext(),"Account created!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
     }
 }
