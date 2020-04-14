9a603930313382f962849d97c79cee9c9e2bcb80
Ali Zaidi <azaidi3@wisc.edu>
2020-04-13 20:35:34 -0700

    Added LoginTest.java

---
+++ app/src/test/java/com/example/badgerbudget/LoginTest.java
@@ -0,0 +1,23 @@
+package com.example.badgerbudget;
+
+import com.example.badgerbudget.data.model.Client;
+import com.example.badgerbudget.ui.login.LoginActivity;
+
+import org.junit.Test;
+import static java.lang.Boolean.FALSE;
+import static org.junit.Assert.*;
+
+public class LoginTest extends LoginActivity {
+    @Test
+    public void SuccesfulLogin(){
+        LoginActivity a = new LoginActivity();
+        Client client = new Client(6868,"10.0.2.2");
+        assertEquals("true", client.sendMessage("login;"+ "dummy" + " "+"password"));
+    }
+    @Test
+    public void UnsuccesfulLogin(){
+        LoginActivity a = new LoginActivity();
+        Client client = new Client(6868,"10.0.2.2");
+        assertEquals("Invalid Combination of Username and Password", client.sendMessage("login;"+ "NotARealAccount" + " "+"password"));
+    }
+}
