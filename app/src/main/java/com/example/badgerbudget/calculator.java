835138f16c9e196ec1e653d628cf73454a40d51e
younanna <anna981209@gmail.com>
2020-04-20 07:19:02 -0500

    calculator page created
    still need JUnit test

--- app/src/main/java/com/example/badgerbudget/calculator.java
+++ app/src/main/java/com/example/badgerbudget/calculator.java
@@ -13,11 +13,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 public class calculator extends AppCompatActivity {
 
     private double userPrice;
-    private double perc;
+    private double tipPerc, taxPerc;
     private String state;
     private double tipAmount;
     private double taxAmount;
@@ -25,24 +34,66 @@ public class calculator extends AppCompatActivity {
     private Spinner tip, tax;
     private Button btn_tip, btn_tax;
 
-    EditText text_amount_tip;
-    TextView tipDisp;
+    EditText text_amount_tip, text_amount_tax;
+    TextView tipDisp, taxDisp;
+    TextView taxPercDisp;
 
-    private double double_amount_tip;
+
+    List<String> statesList = new ArrayList<String>();
+    List<Double> taxPercList = new ArrayList<Double>();
+    Map<String, Double> stateTaxList = new HashMap<String,Double>();
+
+
+    private double double_amount_tip, double_amount_tax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-
+        setStateTax();
         showDropDown();
         getTipPerc();
+        getTaxPerc();
+
 
     }
 
+    public void setStateTax(){
+        taxPercDisp = (TextView) findViewById(R.id.taxPercDisp);
+        String data = "";
+        InputStream is = this.getResources().openRawResource(R.raw.statetax);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        if(is != null){
+            try{
+                while((data = reader.readLine()) != null){
+                    // get state
+                    statesList.add(data);
+                    data = reader.readLine();
+                    // tax percentage
+                    taxPercList.add(Double.valueOf(data));
+                    // empty line
+                    reader.readLine();
+                }
+                is.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        // if lists are filled after above code
+        if(!(statesList.isEmpty() && taxPercList.isEmpty())){
+            for(int i=0; i < statesList.size(); i++){
+                stateTaxList.put(statesList.get(i), taxPercList.get(i));
+            }
+        }
+    }
+
+
     private void showDropDown(){
 
+        // tip dropdown
         tip = (Spinner) findViewById(R.id.tipDropDown);
         ArrayAdapter<String> tipAdapter = new ArrayAdapter<String>(calculator.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Tip));
@@ -50,38 +101,96 @@ public class calculator extends AppCompatActivity {
         // show adapter in the spinner
         tip.setAdapter(tipAdapter);
 
+        // tax dropdown
+        tax = (Spinner) findViewById(R.id.taxDropDown);
+        ArrayAdapter<String> taxAdapter = new ArrayAdapter<String>(calculator.this,
+                android.R.layout.simple_list_item_1, statesList);
+        taxAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // show adapter in the spinner
+        tax.setAdapter(taxAdapter);
+
     }
 
     public void onClick_tip(View view){
 
         // get user input amount
         text_amount_tip = (EditText) findViewById(R.id.enterCost1);
-        String string_amount_tip = text_amount_tip.getText().toString();
+        String string_amount_tip = "";
+        if(isEmpty(text_amount_tip)){
+            string_amount_tip = "0.0";
+        } else {
+            string_amount_tip = text_amount_tip.getText().toString();
+        }
+
         double_amount_tip = Double.valueOf(string_amount_tip);
 
-
-        tipAmount = double_amount_tip * (0.01 *perc);
+        tipAmount = double_amount_tip * tipPerc;
 
         tipDisp = (TextView) findViewById(R.id.tipDisp);
-        tipDisp.setText(String.valueOf(tipAmount));
+        tipDisp.setText(String.format("%.3f", tipAmount));
 
     }
 
+    private boolean isEmpty(EditText eText) {
+
+        return eText.getText().toString().trim().length() == 0;
+    }
+
     public void onClick_tax(View view){
+
+        text_amount_tax = (EditText) findViewById(R.id.enterCost2);
+        String string_amount_tax = "";
+
+        if(isEmpty(text_amount_tax)){
+            string_amount_tax = "0.0";
+        } else {
+            string_amount_tax = text_amount_tax.getText().toString();
+        }
+        double_amount_tax = Double.valueOf(string_amount_tax);
+        taxAmount = double_amount_tax * taxPerc;
+
+        taxPercDisp.setText(String.format("%.3f", taxPerc) + "%");
+        taxDisp = (TextView) findViewById(R.id.taxDisp);
+        taxDisp.setText(String.format("%.3f", taxAmount));
 
     }
 
 
      //get the selected dropdown list value
-    private void getTipPerc() {
+    public void getTipPerc() {
         tip.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String tempTip = tip.getSelectedItem().toString();
                 tempTip = tempTip.substring(0, tempTip.length() - 1);
-                perc = Double.valueOf(tempTip);
+                tipPerc = 0.01 * Double.valueOf(tempTip);
 
                 Toast.makeText(getApplicationContext(), "SELECTED " + tempTip, Toast.LENGTH_SHORT). show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+
+    /* get tax percent
+    https://www.salestaxinstitute.com/resources/rates
+     */
+    public void getTaxPerc() {
+
+        tax.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tempState = tax.getSelectedItem().toString();
+                Set<String> keys = stateTaxList.keySet();
+                for(String key : keys){
+                    if(tempState.equals(key)){
+                        taxPerc = 0.01 * Double.valueOf(stateTaxList.get(key));
+                        break;
+                    }
+                }
+                Toast.makeText(getApplicationContext(), "SELECTED " + taxPerc, Toast.LENGTH_SHORT). show();
             }
 
             @Override
@@ -89,10 +198,7 @@ public class calculator extends AppCompatActivity {
 
             }
         });
-
     }
-
-
 
 
 
---
+++ app/src/test/java/com/example/badgerbudget/calculatorTest.java
@@ -0,0 +1,30 @@
+package com.example.badgerbudget;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class calculatorTest extends calculator {
+
+    @Test
+    public void setStateTax() {
+    }
+
+    @Test
+    public void onClick_tip() {
+    }
+
+    @Test
+    public void onClick_tax() {
+    }
+
+    @Test
+    public void getTaxPerc() {
+
+    }
+
+    @Test
+    public void getTipPerc() {
+
+    }
+}
